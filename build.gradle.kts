6639ba64de68fbaa27ae06728156caee2af2a732
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-09-13 12:11:22 +0300

    arch: add base files

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    kotlin("plugin.serialization") version "2.0.20"
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.kotlin.serialization)
+    }
+}
