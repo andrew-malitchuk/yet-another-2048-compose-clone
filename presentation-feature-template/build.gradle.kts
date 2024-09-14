6226ef8c914ba2b2121cb1e85a9c18e99a67136b
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-09-14 12:13:05 +0300

    arch: add presentation-feature-template

---
+++ presentation-feature-template/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("dev.ya2048cc.convention.feature")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+        }
+    }
+}
