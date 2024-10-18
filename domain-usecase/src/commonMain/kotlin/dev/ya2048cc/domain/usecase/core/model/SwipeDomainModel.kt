a0756b13c43c37a478dfbd656f92a6f132b81554
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-10-18 19:30:49 +0300

    feat: add domain-usecase

---
+++ domain-usecase/src/commonMain/kotlin/dev/ya2048cc/domain/usecase/core/model/SwipeDomainModel.kt
@@ -0,0 +1,7 @@
+package dev.ya2048cc.domain.usecase.core.model
+
+import dev.ya2048cc.domain.usecase.core.model.base.BaseDomainModel
+
+enum class SwipeDomainModel : BaseDomainModel {
+    LEFT, RIGHT, UP, DOWN
+}
