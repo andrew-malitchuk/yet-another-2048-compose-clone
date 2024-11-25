82ba2e36361f418eabf37fb82cbf416a616ebba2
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-11-25 20:13:20 +0200

    feat: add theme

---
+++ presentation-core-ui/src/commonMain/kotlin/dev/ya2048cc/presentation/core/ui/source/style/core/provider/Provider.kt
@@ -0,0 +1,35 @@
+package dev.ya2048cc.presentation.core.ui.source.style.core.provider
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import dev.ya2048cc.presentation.core.ui.source.style.core.artifact.YacrukColor
+import dev.ya2048cc.presentation.core.ui.source.style.core.artifact.YacrukFontSize
+import dev.ya2048cc.presentation.core.ui.source.style.core.artifact.YacrukLineHeight
+import dev.ya2048cc.presentation.core.ui.source.style.core.artifact.YacrukSpacing
+import dev.ya2048cc.presentation.core.ui.source.style.core.artifact.YacrukTypography
+
+internal val LocalYacrukColors =
+    staticCompositionLocalOf<YacrukColor> {
+        error("No implementation")
+    }
+
+@Suppress("unused")
+internal val LocalYacrukFontSize =
+    staticCompositionLocalOf<YacrukFontSize> {
+        error("No implementation")
+    }
+
+@Suppress("unused")
+internal val LocalYacrukLineHeight =
+    staticCompositionLocalOf<YacrukLineHeight> {
+        error("No implementation")
+    }
+
+internal val LocalYacrukSpacing =
+    staticCompositionLocalOf<YacrukSpacing> {
+        error("No implementation")
+    }
+
+internal val LocalYacrukTypography =
+    staticCompositionLocalOf<YacrukTypography> {
+        error("No implementation")
+    }
