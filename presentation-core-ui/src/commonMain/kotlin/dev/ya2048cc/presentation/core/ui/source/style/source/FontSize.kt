a336295ecb17fa9d046e0c0a520c6da9a9ade475
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-11-26 20:13:31 +0200

    feat: add theme

---
+++ presentation-core-ui/src/commonMain/kotlin/dev/ya2048cc/presentation/core/ui/source/style/source/FontSize.kt
@@ -0,0 +1,15 @@
+package dev.ya2048cc.presentation.core.ui.source.style.source
+
+import androidx.compose.ui.unit.sp
+import dev.ya2048cc.presentation.core.ui.source.style.core.artifact.YacrukFontSize
+
+val fontSize =
+    YacrukFontSize(
+        display = 34.sp,
+        headline = 24.sp,
+        title = 20.sp,
+        subHeading = 16.sp,
+        body = 14.sp,
+        caption = 12.sp,
+        button = 14.sp,
+    )
---
+++ presentation-core-ui/src/commonMain/kotlin/dev/ya2048cc/presentation/core/ui/source/style/source/LineHeight.kt
@@ -0,0 +1,15 @@
+package dev.ya2048cc.presentation.core.ui.source.style.source
+
+import androidx.compose.ui.unit.sp
+import dev.ya2048cc.presentation.core.ui.source.style.core.artifact.YacrukLineHeight
+
+val lineHeight =
+    YacrukLineHeight(
+        display = 40.sp,
+        headline = 32.sp,
+        title = 26.sp,
+        subHeading = 24.sp,
+        body = 20.sp,
+        caption = 16.sp,
+        button = 16.sp,
+    )
