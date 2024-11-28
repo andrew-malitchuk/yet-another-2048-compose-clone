2d8ca07d26c6ff9aaaceede3afdace8738c631cd
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-11-28 20:14:33 +0200

    feat: add presentation-feature-host

---
+++ presentation-feature-host/build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_21)
+                }
+            }
+        }
+    }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "presentation-feature-host"
+            isStatic = true
+        }
+    }
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.navigation.compose)
+            implementation(libs.kotlinx.serialization.json.v150)
+
+            implementation(project(":presentation-core-navigation"))
+            implementation(project(":presentation-feature-game"))
+            implementation(project(":presentation-feature-about"))
+            implementation(project(":presentation-core-ui"))
+        }
+    }
+}
+
+android {
+    compileSdk = 34
+    namespace = "dev.ya2048cc.presentation.feature.host"
+    defaultConfig {
+        minSdk = 24
+    }
+       compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        jvmToolchain(21)
+    }
+}
+
